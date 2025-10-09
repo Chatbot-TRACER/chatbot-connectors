247de2dec6a8c17a8301038292b185d0c8383cf4
LittleHaku <ivandelhorno@gmail.com>
2025-10-09 14:05:25 +0200

    botlovers bot

--- chatbot_connectors/implementations/__init__.py
+++ chatbot_connectors/implementations/__init__.py
@@ -1,11 +1,13 @@
 """Chatbot implementation modules."""
 
+from .botlovers import BotloversChatbot
 from .custom import CustomChatbot
 from .millionbot import MillionBot
 from .rasa import RasaChatbot
 from .taskyto import ChatbotTaskyto
 
 __all__ = [
+    "BotloversChatbot",
     "ChatbotTaskyto",
     "CustomChatbot",
     "MillionBot",
