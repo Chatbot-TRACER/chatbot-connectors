46386a929a51d5336d3bd3451e5e7fc2680b216d
newi999 <alexdpe99@gmail.com>
2026-05-21 17:34:22 +0200

    format

--- chatbot_connectors/implementations/millionbot.py
+++ chatbot_connectors/implementations/millionbot.py
@@ -164,8 +164,7 @@ class MillionBot(Chatbot):
             "Content-Type": "application/json",
             "Authorization": f"API-KEY {self.millionbot_config.api_key}",
             "User-Agent": (
-                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                "AppleWebKit/537.36 (KHTML, like Gecko) Chrome Safari/537.36"
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome Safari/537.36"
             ),
         }
         if origin:
